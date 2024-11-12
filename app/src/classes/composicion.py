8e056f70fbbc937c1a4fabcefe512b177e8c5378
Marcosis2 <164145960+Marcosis2@users.noreply.github.com>
2024-11-12 19:31:20 +0100

    added get_all_compositions

--- app/src/classes/composicion.py
+++ app/src/classes/composicion.py
@@ -115,3 +115,18 @@ class ComposicionDAO:
             return []
         finally:
             conn.close()
+
+    
+     # Obtener todas las composiciones publicadas
+    def get_all_composiciones(self):
+        try:
+            conn = sqlite3.connect(DB_PATH)
+            cursor = conn.cursor()
+            cursor.execute('''SELECT * FROM Composicion_TAB ''')
+            rows = cursor.fetchall()
+            return [ComposicionVO(row[0], row[1], row[2], row[3], row[4]) for row in rows]
+        except sqlite3.Error as e:
+            print(f"An error occurred: {e}")
+            return []
+        finally:
+            conn.close()
