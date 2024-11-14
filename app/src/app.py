49c3f0188b318c698f22094fa9858606c40fefc4
rachel-cd-04 <875001@unizar.es>
2024-11-14 20:11:18 +0100

    solo falta video

--- app/src/app.py
+++ app/src/app.py
@@ -1,5 +1,4 @@
 from flask import Flask, redirect, render_template, request, session, jsonify
-#from django.contrib.auth.decorators import login_required
 from classes.usuario import UsuarioDAO, UsuarioVO
 from classes.campeon import CampeonDAO, CampeonVO
 from classes.composicion import ComposicionDAO, ComposicionVO
